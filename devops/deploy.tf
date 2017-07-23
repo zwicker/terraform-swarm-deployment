37b534485ac06d193df0d10bcbce1f90af918760
zwicker <emmetmemery@Emmets-MBP.lan>
2017-07-23 22:56:00 +1000

    Bug Fix: Fix missing commas from provisioning scripts

--- devops/deploy.tf
+++ devops/deploy.tf
@@ -93,7 +93,7 @@ resource "aws_instance" "master" {
     inline = [
       "sudo apt-get update",
       "sudo apt-get install apt-transport-https ca-certificates curl software-properties-common",
-      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -"
+      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
       "sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\"",
       "sudo apt-get update",
       "sudo apt-get install -y docker-ce=17.06.0~ce-0~ubuntu-xenial",
@@ -131,7 +131,7 @@ resource "aws_instance" "slave" {
     inline = [
       "sudo apt-get update",
       "sudo apt-get install apt-transport-https ca-certificates curl software-properties-common",
-      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -"
+      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
       "sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\"",
       "sudo apt-get update",
       "sudo apt-get install -y docker-ce=17.06.0~ce-0~ubuntu-xenial",
